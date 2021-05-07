031ede59531bd89f0b0de708ae2b235bd5d78558
geracimov <geracimov@gmail.com>
2021-05-07 14:36:47 +0300

    javadoc

--- src/main/java/ru/geracimov/leetcode/top_interview_questions/easy/strings/ReverseInteger.java
+++ src/main/java/ru/geracimov/leetcode/top_interview_questions/easy/strings/ReverseInteger.java
@@ -1,5 +1,15 @@
 package ru.geracimov.leetcode.top_interview_questions.easy.strings;
 
+/**
+ * Reverse Integer
+ * <p>Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside
+ * the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).</p>
+ * Constraints:
+ * <ul>
+ *   <li>-2^31 <= x <= 2^31 - 1</li>
+ * </ul>
+ */
 public class ReverseInteger {
     private static final int PRE_MAX_VALUE = Integer.MAX_VALUE / 10;
     private static final int PRE_MIN_VALUE = Integer.MIN_VALUE / 10;
--- src/main/java/ru/geracimov/leetcode/top_interview_questions/easy/strings/ReverseString.java
+++ src/main/java/ru/geracimov/leetcode/top_interview_questions/easy/strings/ReverseString.java
@@ -1,5 +1,14 @@
 package ru.geracimov.leetcode.top_interview_questions.easy.strings;
 
+/**
+ * Reverse String
+ * <p>Write a function that reverses a string. The input string is given as an array of characters s.</p>
+ * Constraints:
+ *<ul>
+ *     <li>1 <= s.length <= 10^5</li>
+ *     <li>s[i] is a printable ascii character.</li>
+ *     </ul>
+ */
 public class ReverseString {
     public void reverseString(char[] s) {
         char temp;
