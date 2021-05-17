6cdc0846f619f8682e0afb1375e09dee7c50c3bf
geracimov <geracimov@gmail.com>
2021-05-17 23:32:43 +0300

    Rotate Array

---
+++ src/main/java/ru/geracimov/leetcode/top_interview_questions/easy/array/RotateArray.java
@@ -0,0 +1,17 @@
+package ru.geracimov.leetcode.top_interview_questions.easy.array;
+
+/**
+ * Rotate Array
+ * <p>iven an array, rotate the array to the right by k steps, where k is non-negative.</p>
+ * Constraints:
+ * <ul>
+ *     <li>1 <= nums.length <= 10^5</li>
+ *     <li>-2^31 <= nums[i] <= 2^31 - 1</li>
+ *     <li>0 <= k <= 10^5</li>
+ * </ul>
+ */
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+
+    }
+}
