52daf2c4c9d73ce8eef9f564ac2904464264f2fb
geracimov <geracimov@gmail.com>
2021-06-01 00:46:41 +0300

    Swap Nodes in Pairs

--- src/main/java/ru/geracimov/leetcode/recursion/SwapNodesInPairs.java
+++ src/main/java/ru/geracimov/leetcode/recursion/SwapNodesInPairs.java
@@ -1,16 +1,25 @@
 package ru.geracimov.leetcode.recursion;
 
+/**
+ * Swap Nodes in Pairs
+ * <p>
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * You must solve the problem without modifying the values in the list's nodes
+ * (i.e., only nodes themselves may be changed.)
+ */
 public class SwapNodesInPairs {
     public ListNode swapPairs(ListNode head) {
-        ListNode current = new ListNode(0, head);
+        var current = new ListNode(0, head);
+        var res = current;
         ListNode first;
         ListNode second;
         while ((first = current.next) != null && (second = current.next.next) != null) {
+            first.next = second.next;
+            second.next = first;
             current.next = second;
-            current.next.next = first;
+            current = current.next.next;
         }
-        ListNode.print(head);
-        return current.next;
+        return res.next;
     }
 }
 
@@ -34,14 +43,15 @@ class ListNode {
     }
 
     static void print(ListNode head) {
-        while (head.next != null) {
+        do {
             System.out.print(head);
-        }
-        System.out.println();
+            head = head.next;
+        } while (head.next != null);
+        System.out.println(head);
     }
 
     @Override
     public String toString() {
-        return String.valueOf("(" + val + ")");
+        return "(" + val + ")";
     }
 }
--- src/test/java/ru/geracimov/leetcode/recursion/SwapNodesInPairsTest.java
+++ src/test/java/ru/geracimov/leetcode/recursion/SwapNodesInPairsTest.java
@@ -2,11 +2,27 @@ package ru.geracimov.leetcode.recursion;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SwapNodesInPairsTest {
+    private final SwapNodesInPairs swapNodesInPairs = new SwapNodesInPairs();
 
     @Test
     void swapPairs() {
+        ListNode node4 = new ListNode(4, null);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+        ListNode.print(node1);
+        ListNode swap = swapNodesInPairs.swapPairs(node1);
+        ListNode.print(swap);
+        assertThat(swap).hasFieldOrPropertyWithValue("val", 2)
+                .hasFieldOrPropertyWithValue("next", node1);
+        assertThat(swap.next).hasFieldOrPropertyWithValue("val", 1)
+                .hasFieldOrPropertyWithValue("next", node4);
+        assertThat(swap.next.next).hasFieldOrPropertyWithValue("val", 4)
+                .hasFieldOrPropertyWithValue("next", node3);
+        assertThat(swap.next.next.next).hasFieldOrPropertyWithValue("val", 3)
+                .hasFieldOrPropertyWithValue("next", null);
     }
 }
