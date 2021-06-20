5a3f34638fbfb232ed2ff153ae9ea761e8160080
geracimov <geracimov@gmail.com>
2021-06-20 23:32:31 +0300

    Remove Nth Node From End Of List test

---
+++ src/main/java/ru/geracimov/leetcode/top_interview_questions/easy/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,36 @@
+package ru.geracimov.leetcode.top_interview_questions.easy.linkedlist;
+
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    '}';
+        }
+    }
+}
