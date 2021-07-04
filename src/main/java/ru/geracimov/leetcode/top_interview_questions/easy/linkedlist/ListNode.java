a0e57b1fd78734792a894059a9a62a8af4083ff0
geracimov <geracimov@gmail.com>
2021-07-04 22:03:14 +0300

    ListNode extracted

---
+++ src/main/java/ru/geracimov/leetcode/top_interview_questions/easy/linkedlist/ListNode.java
@@ -0,0 +1,43 @@
+package ru.geracimov.leetcode.top_interview_questions.easy.linkedlist;
+
+import java.util.StringJoiner;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        var listNode = (ListNode) o;
+
+        return val == listNode.val;
+    }
+
+    @Override
+    public int hashCode() {
+        return val;
+    }
+
+    void print() {
+        var joiner = new StringJoiner("->");
+        ListNode node = this;
+        do {
+            joiner.add("" + node.val);
+        } while ((node = node.next) != null);
+        System.out.println(joiner);
+    }
+}
