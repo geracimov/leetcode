c2ae905dab8e5395985d78ee625582be5537f6d7
geracimov <geracimov@gmail.com>
2021-07-19 23:55:39 +0300

    Valid Mountain Array

--- src/main/java/ru/geracimov/leetcode/arrays101/ValidMountainArray.java
+++ src/main/java/ru/geracimov/leetcode/arrays101/ValidMountainArray.java
@@ -6,18 +6,19 @@ package ru.geracimov.leetcode.arrays101;
  */
 public class ValidMountainArray {
     public boolean validMountainArray(int[] arr) {
-        int n = arr.length;
+        int len = arr.length;
+        int last = len - 1;
         var i = 0;
 
-        while (i + 1 < n && arr[i] < arr[i + 1])
+        while (i + 1 < len && arr[i] < arr[i + 1])
             i++;
 
-        if (i == 0 || i == n - 1)
+        if (i == 0 || i == last)
             return false;
 
-        while (i + 1 < n && arr[i] > arr[i + 1])
+        while (i + 1 < len && arr[i] > arr[i + 1])
             i++;
 
-        return i == n - 1;
+        return i == last;
     }
 }
