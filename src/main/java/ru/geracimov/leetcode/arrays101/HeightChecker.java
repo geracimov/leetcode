59b2aee7cff481563be228834c6b54744812a886
geracimov <geracimov@gmail.com>
2021-07-22 00:06:24 +0300

    Height Checker

--- src/main/java/ru/geracimov/leetcode/arrays101/HeightChecker.java
+++ src/main/java/ru/geracimov/leetcode/arrays101/HeightChecker.java
@@ -5,13 +5,11 @@ import java.util.Arrays;
 public class HeightChecker {
     public int heightChecker(int[] heights) {
         if (heights == null || heights.length < 2) return 0;
-//        var copy = new int[heights.length];
-//        System.arraycopy(heights, 0, copy, 0, heights.length);
-        int[] i = new int[1];
-        int[] ints = Arrays.stream(heights).sorted().toArray();
-        return (int) Arrays.stream(heights).sorted()
-                .filter(v -> v != heights[i[0]++])
-                .count();
-
+        int[] i = new int[2];
+        Arrays.stream(heights).sorted()
+                .forEach(v -> {
+                    if (v != heights[i[0]++]) i[1]++;
+                });
+        return i[1];
     }
 }
--- src/test/java/ru/geracimov/leetcode/arrays101/HeightCheckerTest.java
+++ src/test/java/ru/geracimov/leetcode/arrays101/HeightCheckerTest.java
@@ -11,6 +11,8 @@ class HeightCheckerTest {
     void heightChecker() {
         assertThat(test.heightChecker(new int[]{5, 2, 3})).isEqualTo(3);
         assertThat(test.heightChecker(new int[]{1, 2, 3})).isZero();
-        assertThat(test.heightChecker(new int[]{2, 2, 3})).isEqualTo(2);
+        assertThat(test.heightChecker(new int[]{1, 1, 4, 2, 1, 3})).isEqualTo(3);
+        assertThat(test.heightChecker(new int[]{5, 1, 2, 3, 4})).isEqualTo(5);
+        assertThat(test.heightChecker(new int[]{1, 2, 3, 4, 5})).isZero();
     }
 }
