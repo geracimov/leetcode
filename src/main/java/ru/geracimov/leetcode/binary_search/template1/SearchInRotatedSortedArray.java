29a99f7659a88ffea74f25a3e8b9ecab3460610b
geracimov <geracimov@gmail.com>
2021-08-09 23:51:53 +0300

    Search in Rotated Sorted Array

---
+++ src/main/java/ru/geracimov/leetcode/binary_search/template1/SearchInRotatedSortedArray.java
@@ -0,0 +1,19 @@
+package ru.geracimov.leetcode.binary_search.template1;
+
+/**
+ * <h1>Search in Rotated Sorted Array</h1>
+ * <p>There is an integer array nums sorted in ascending order (with distinct values).
+ * <p>
+ * Prior to being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+ * <p>
+ * Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+ * <p>
+ * You must write an algorithm with O(log n) runtime complexity.</p>
+ */
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int length = nums.length;
+
+        return target + length;
+    }
+}
