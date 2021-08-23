9567b9f07703c4c804874e84aaa1499be7499d79
geracimov <geracimov@gmail.com>
2021-08-23 23:32:57 +0300

    gitignore target

---
+++ src/main/java/ru/geracimov/leetcode/binary_search/template3/SearchForRange.java
@@ -0,0 +1,7 @@
+package ru.geracimov.leetcode.binary_search.template3;
+
+public class SearchForRange {
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{nums.length + target};
+    }
+}
